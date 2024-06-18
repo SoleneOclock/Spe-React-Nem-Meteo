ea1da7b53bc72eb6fd1e8c686153e5df7b464ed7
SoleneOclock <solene@oclock.io>
2024-06-18 14:35:11 +0200

    ajout fichiers env

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,9 @@ import Form from '../Form/Form';
 import './App.scss';
 
 function App() {
-  console.log(import.meta.env.MODE);
+  // vite va cherche le password dan sle fichier local or ce fichier n'est pas commité
+  // mais on a ajouté un .env.exemple pour que toi dev collegue si tu recupere mon code tu sache quelle variable ajouter à ton .env.local pour que ça marche
+  console.log(import.meta.env.VITE_PASSWORD_API);
 
   return (
     <div className="app">
--- src/components/CityList/City.tsx
+++ src/components/CityList/City.tsx
@@ -1,13 +1,14 @@
 import { ICity } from '../../@types/city';
 
 export default function City({ city }: { city: ICity }) {
+  // on recupere la valeur d'une variable d'environement dans import.meta.env (c'est avec vite ) si on est avec next il les place dans une varibale appelée process.env.
+
+  const apiURL = import.meta.env.VITE_BASE_URL;
+
   return (
     <div className="city">
       <h2>{city.name}</h2>
-      <img
-        src={`http://openweathermap.org/img/wn/${city.icon}@2x.png`}
-        alt="nuageux"
-      />
+      <img src={`${apiURL}img/wn/${city.icon}@2x.png`} alt="nuageux" />
       <h3>{city.temp}°C</h3>
     </div>
   );
