8399178790750da14cfe6e9eba43fdd0460f6820
SoleneOclock <solene@oclock.io>
2024-06-18 14:10:57 +0200

    ajout de l'afochage de l'environnement

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,9 +3,11 @@ import Form from '../Form/Form';
 import './App.scss';
 
 function App() {
+  console.log(import.meta.env.MODE);
+
   return (
     <div className="app">
-      <h1>Widget Meteo de Soso </h1>
+      <h1>Widget Meteo de Soso (env : {import.meta.env.MODE})</h1>
       <Form />
       <CityList />
     </div>
